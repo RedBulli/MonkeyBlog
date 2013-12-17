45ca450aeee6dae8cb2860be75c410737a574f21
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2013-12-17 19:57:15 +0200

    Add friends to Monkey

--- MonkeyBlog/models/monkey.py
+++ MonkeyBlog/models/monkey.py
@@ -2,12 +2,22 @@ from MonkeyBlog.extensions import db
 
 from sqlalchemy_utils import EmailType
 
+monkey_friends = db.Table('monkey_friends',
+    db.Column('monkey_id', db.Integer, db.ForeignKey('monkey.id'), primary_key=True),
+    db.Column('friend_id', db.Integer, db.ForeignKey('monkey.id'), primary_key=True)
+)
 
 class Monkey(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(EmailType(), nullable=False, unique=True)
+
+    friends = db.relationship('Monkey', 
+        secondary=monkey_friends,
+        primaryjoin=id==monkey_friends.c.monkey_id,
+        secondaryjoin=id==monkey_friends.c.friend_id
+    )
 
     def __init__(self, name, email):
         self.name = name
--- tests/models/test_monkey.py
+++ tests/models/test_monkey.py
@@ -1,7 +1,11 @@
 from tests import BaseTestCase
 from tests.factories import MonkeyFactory
 
+from pytest import raises
+from sqlalchemy.exc import IntegrityError
+
 from MonkeyBlog.models.monkey import Monkey
+from MonkeyBlog.extensions import db
 
 
 class TestMonkeyColumnExistences(BaseTestCase):
@@ -10,5 +14,35 @@ class TestMonkeyColumnExistences(BaseTestCase):
         monkey = MonkeyFactory()
         self.monkey = Monkey.query.get(monkey.id)
 
+    def test_has_column_id(self):
+        assert self.monkey.id
+
     def test_has_column_name(self):
         assert self.monkey.name
+
+    def test_has_column_email(self):
+        assert self.monkey.email
+
+    def test_has_column_name(self):
+        assert self.monkey.name
+
+
+class TestMonkeyRelationships(BaseTestCase):
+    def setup_method(self, method):
+        super(TestMonkeyRelationships, self).setup_method(method)
+        self.friend = MonkeyFactory(name='Ystava')
+        monkey = MonkeyFactory()
+        self.monkey = Monkey.query.get(monkey.id)
+
+    def test_add_friend(self):
+        assert len(self.monkey.friends) == 0
+        self.monkey.friends.append(self.friend)
+        db.session.commit()
+        assert len(self.monkey.friends) == 1
+        assert self.monkey.friends[0].name == 'Ystava'
+
+    def test_add_same_friend_twice(self):
+        self.monkey.friends.append(self.friend)
+        self.monkey.friends.append(self.friend)
+        with raises(IntegrityError):
+            db.session.commit()
