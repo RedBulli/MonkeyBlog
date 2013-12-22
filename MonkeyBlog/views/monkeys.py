c8ef81bbb64a62f395924042703f28ef1efad7b9
Sampo Verkasalo <sampo.verkasalo@gmail.com>
2013-12-22 16:59:42 +0200

    Refactor monkey views

--- MonkeyBlog/views/monkeys.py
+++ MonkeyBlog/views/monkeys.py
@@ -6,15 +6,37 @@ from MonkeyBlog.forms.monkey_form import MonkeyForm
 from MonkeyBlog.extensions import db
 
 
-def set_form_queries(form, monkey_id=None):
-    form.friends.query = Monkey.query.filter(Monkey.id != monkey_id)
-
-
 class MonkeysView(FlaskView):
+    def _set_form_queries(self, form, monkey=None):
+        id = None
+        if (monkey != None):
+            id = monkey.id
+        form.friends.query = Monkey.query.filter(Monkey.id != id)
+
+    def _get_form(self, monkey=None):
+        form = MonkeyForm(request.form, monkey)
+        self._set_form_queries(form, monkey)
+        return form
+
+    def _create_monkey(self, form):
+        monkey = Monkey()
+        form.populate_obj(monkey)
+        db.session.add(monkey)
+        db.session.commit()
+        return monkey
+
+    def _update_monkey(self, monkey, form):
+        form.populate_obj(monkey)
+        db.session.commit()
+
+    def _delete_monkey(self, id):
+        monkey = Monkey.query.get(id)
+        db.session.delete(monkey)
+        db.session.commit()
+
     def get(self, id):
         monkey = Monkey.query.get(id)
-        form = MonkeyForm(obj=monkey)
-        set_form_queries(form, monkey.id)
+        form = self._get_form(monkey)
         return render_template('monkey_view.html', monkey=monkey, form=form)
 
     def index(self):
@@ -22,38 +44,26 @@ class MonkeysView(FlaskView):
         return render_template('monkey_list.html', monkeys=monkeys)
 
     def create(self):
-        form = MonkeyForm()
-        set_form_queries(form)
+        form = self._get_form()
         return render_template('monkey_create.html', form=form)
 
     def post(self):
-        if not request.form:
-            form = MonkeyForm()
-        else:
-            form = MonkeyForm(request.form)
-        set_form_queries(form)
+        form = self._get_form()
         if not form.validate():
             return render_template('monkey_create.html', form=form)
         else:
-            monkey = Monkey()
-            form.populate_obj(monkey)
-            db.session.add(monkey)
-            db.session.commit()
+            monkey = self._create_monkey(form)
             return redirect(url_for('MonkeysView:get', id=monkey.id))
 
     @route('<id>', methods=['POST'])
     def update(self, id):
         monkey = Monkey.query.get(id)
-        form = MonkeyForm(request.form, monkey)
-        set_form_queries(form, monkey.id)
+        form = self._get_form()
         if form.validate():
-            form.populate_obj(monkey)
-            db.session.commit()
+            self._update_monkey(monkey, form)
         return render_template('monkey_view.html', form=form, monkey=monkey)
 
     @route('<id>/delete', methods=['POST'])
     def destroy(self, id):
-        monkey = Monkey.query.get(id)
-        db.session.delete(monkey)
-        db.session.commit()
+        self._delete_monkey(id)
         return redirect(url_for('MonkeysView:index'))
